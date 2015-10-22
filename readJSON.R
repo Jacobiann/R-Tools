af84a57851f3d40c5b80353cc2c5a0548667a610
Jacobiann <s355338@gmail.com>
2015-10-22 17:17:04 +0800

    2015/10/22 how to read json in R

--- readJSON.R
+++ readJSON.R
@@ -1,7 +1,9 @@
 library(rjson)
+#read data
 needFile <- "C:\\Users\\s3553\\Desktop\\car.json"
 json <- scan(needFile ,what = "character" , sep = "\n")
 title <- function(x) fromJSON(x) [c("air","documentId")]
 data <- lapply(json,title)
+#convert to dataframe
 df <- data.frame(matrix(unlist(data), nrow=length(data), byrow=T))
 colnames(df) <- c("air","documentId")
